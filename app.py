1931c064e244c2fa7b851ce342e670c1643b57c3
Fiorella-Fernandez-Bruno <fiorella.fernandez@outlook.com>
2026-09-12 12:02:14 -0500

    Implement module selection in app
    Add selection for modules in the Streamlit app

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por Fiorella Fernández")
+
 modulos = st.selectbox("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
+
 if modulos == "Listas":
  st.write("Te encuentras en el módulo de Listas")
 elif modulos == "Arreglos":
