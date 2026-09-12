93bf7f3267a45f8575a4f96af38d2bbe65c1ae2a
Fiorella-Fernandez-Bruno <fiorella.fernandez@outlook.com>
2026-09-12 12:11:15 -0500

    Set default values for range inputs in app.py

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ modulos = st.sidebar.selectbox("Seleccione el módulo",["Listas","Arreglos","Fun
 
 if modulos == "Listas":
  st.write("Te encuentras en el módulo de Listas")
- valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
- valor_final = st.number_input("Ingresa tu valor final del rango")
+ valor_inicial = st.number_input("Ingresa tu valor inicial del rango",value=0)
+ valor_final = st.number_input("Ingresa tu valor final del rango",value=10)
+ Lista = list(range(valor_inicial,valor_final))
+
+
 elif modulos == "Arreglos":
   st.write("Te encuentras en el módulo de Arreglos")
 elif modulos == "Funciones":
