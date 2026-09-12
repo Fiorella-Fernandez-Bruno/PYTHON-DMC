551da4624b4403f212cf4b68c58a612490f638ec
Fiorella-Fernandez-Bruno <fiorella.fernandez@outlook.com>
2026-09-12 11:58:42 -0500

    Fix syntax errors in module selection logic

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por Fiorella Fernández")
 modulos = st.selectbox("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
-if modulos : "Listas":
+if modulos "Listas":
  st.write("Te encuentras en el módulo de Listas")
-elif modulos : "Arreglos":
+elif modulos "Arreglos":
   st.write("Te encuentras en el módulo de Arreglos")
 elif modulos "Funciones":
   st.write("Te encuentras en el módulo de Funciones")
