f9f18a03b41a813ae3d95f9c83295f51e333c5b9
Fiorella-Fernandez-Bruno <fiorella.fernandez@outlook.com>
2026-09-12 11:50:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por Fiorella Fernández")
+modulos = st.selectbox("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
