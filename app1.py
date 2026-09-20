9fe0a3aaf97a7d73a4188f7dac9038a876559e7a
Fiorella-Fernandez-Bruno <fiorella.fernandez@outlook.com>
2026-09-20 07:21:27 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from libreria_funciones_proyecto1 import calcular_margen_neto
+from libreria_clases_proyecto1 import InventarioProducto
 
+st.set_page_config(page_tittle="Proyecto 1 - Python Fundamentals")
 
