0a5ec3ff084c44296f2f04103b316eb49570f382
Fiorella-Fernandez-Bruno <fiorella.fernandez@outlook.com>
2026-09-20 07:29:30 -0500

    Set page configuration for Streamlit app

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import pandas as pd
 from libreria_funciones_proyecto1 import calcular_margen_neto
 from librería_clases_proyecto1 import InventarioProducto
 
-
+st.set_page_config(page_title="Proyecto 1 - Python Fundamentals", page_icon="🐍")
